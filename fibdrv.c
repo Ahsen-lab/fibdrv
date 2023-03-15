ceb4eb4583cc488805ef9eaaab16a27713c8129f
Ahsen <ahsenlaboratory@gmail.com>
2023-03-15 10:23:23 +0800

    Fix VLA warning in kernel build

--- fibdrv.c
+++ fibdrv.c
@@ -26,17 +26,17 @@ static DEFINE_MUTEX(fib_mutex);
 
 static long long fib_sequence(long long k)
 {
+    if (k < 1)
+        return 0;
+
     /* FIXME: C99 variable-length array (VLA) is not allowed in Linux kernel. */
-    long long f[k + 2];
+    long long f[2] = {1, 1};
 
-    f[0] = 0;
-    f[1] = 1;
-
-    for (int i = 2; i <= k; i++) {
-        f[i] = f[i - 1] + f[i - 2];
+    for (int i = 2; i < k; i++) {
+        f[1] += f[0];
+        f[0] = f[1] - f[0];
     }
-
-    return f[k];
+    return f[1];
 }
 
 static int fib_open(struct inode *inode, struct file *file)
